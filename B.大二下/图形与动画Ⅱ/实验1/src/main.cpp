77fc423241e776ac686087be39eaa823ace1b53c
star-ful <163505536+star-ful@users.noreply.github.com>
2024-12-21 17:57:14 +0800

    Delete B.大二下 directory

--- B.大二下/图形与动画Ⅱ/实验1/src/main.cpp
+++
@@ -1,6 +0,0 @@
-#include "lab1.h"
-
-int main(int argc, char** argv)
-{
-	LAB1::lab1(argc, argv);
-}
--- B.大二下/图形与动画Ⅱ/实验2/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "lab2.h"
-
-
-int main(int argc, char** argv)
-{	
-	LAB2_1::lab2(argc, argv); // 雪人世界
-	//LAB2_2::lab2(argc, argv); // 旋转正方体
-}
